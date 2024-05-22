12534e02fa675a632464923b183763e646b791d5
Vladislav Glukhovskiy <vlad.gluhovskiy_18376@mail.ru>
2024-05-22 09:35:51 +0700

    Поменял данные в модели без миграции

--- models/models.py
+++ models/models.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timezone
-from typing import List
 from sqlalchemy import TIMESTAMP, ForeignKey, Integer
 from sqlalchemy import String
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "user"
@@ -15,19 +15,18 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(30), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     registered_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
-    #двунаправленная связь между пользователями и зарплатами
-    salaries: Mapped[List["Salary"]] = relationship("Salary", back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
 
 class Salary(Base):
     __tablename__ = "salary"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    #Связка с User
+    # Связка с User
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"))
     current_salary: Mapped[int] = mapped_column(Integer, nullable=False)
-    next_raise_date: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
-    # двунаправленная связь между зарплатами и пользователями
-    user: Mapped["User"] = relationship("User", back_populates="salaries")
+    next_raise_date: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), nullable=False
+    )

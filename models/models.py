f28cdc46d65806264617d7cf637591f45145cd98
Vladislav Glukhovskiy <vlad.gluhovskiy_18376@mail.ru>
2024-05-23 18:37:46 +0700

    Добавил менеджера, схемы, ендпоинты к роутеру

--- models/models.py
+++ models/models.py
@@ -1,33 +1,48 @@
 from datetime import datetime, timezone
-from sqlalchemy import TIMESTAMP, ForeignKey, Integer
-from sqlalchemy import String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import TIMESTAMP, Boolean, ForeignKey, Integer, String
+from sqlalchemy.orm import declarative_base, relationship, mapped_column, Mapped
 
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = "user"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
-    email: Mapped[str] = mapped_column(nullable=False)
-    username: Mapped[str] = mapped_column(String(30), nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
     registered_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
 
+    salaries = relationship(
+        "Salary", back_populates="user", cascade="all, delete-orphan"
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+
+
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+        return f"User(id={self.id!r}, name={self.name!r}, username={self.username!r})"
 
 
 class Salary(Base):
     __tablename__ = "salary"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    # Связка с User
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"))
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("user.id", ondelete="CASCADE")
+    )
     current_salary: Mapped[int] = mapped_column(Integer, nullable=False)
     next_raise_date: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True), nullable=False
     )
+
+    user = relationship("User", back_populates="salaries")
+
+    def __repr__(self) -> str:
+        return f"Salary(id={self.id!r}, user_id={self.user_id!r}, current_salary={self.current_salary!r}, next_raise_date={self.next_raise_date!r})"
--- src/main.py
+++ src/main.py
@@ -1,25 +1,43 @@
-from fastapi import FastAPI
+from typing import Annotated
 
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from fastapi_users import FastAPIUsers, fastapi_users
+from fastapi_users.authentication import Authenticator
 import uvicorn
-
-from users.views import router as users_router
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserCreate, UserRead
+from src.auth.auth import auth_backend
+from src.database import User
 
 app = FastAPI()
-app.include_router(users_router)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+authenticator = Authenticator([auth_backend], get_user_manager)
 
 
-@app.get("/")
-def hello_index():
-    return {
-        "message": "Hello index!",
-    }
+# @app.post("/auth/jwt/login/")
+# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+#     return {"token": token}
 
 
-@app.get("/hello/")
-def hello(name: str = "World"):
-    name = name.strip().title()
-    return {"message": f"Hello {name}!"}
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
